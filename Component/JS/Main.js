a4384a22af6b6872ed5dfb2fb9481dddd2439361
singhkaushik <singhkaushik28@gmail.com>
2022-12-19 20:01:05 +0530

    home

---
+++ Component/JS/Main.js
@@ -0,0 +1,10 @@
+// function myFunction(x) {
+//   x.classList.toggle("change");
+//   // let x = document.getElementById("nav");
+//   // if (x.style.display === "none") {
+//   //   x.style.display = "block";
+//   // } else {
+//   //   x.style.display = "none";
+//   // }
+// }
+function pop() {}
