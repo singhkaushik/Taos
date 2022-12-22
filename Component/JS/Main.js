2b01b7108e4c1bac15a257cfcb5236d206f6710c
singhkaushik <singhkaushik28@gmail.com>
2022-12-22 14:34:33 +0530

    ok

--- Component/JS/Main.js
+++ Component/JS/Main.js
@@ -1,10 +1,31 @@
-// function myFunction(x) {
-//   x.classList.toggle("change");
-//   // let x = document.getElementById("nav");
-//   // if (x.style.display === "none") {
-//   //   x.style.display = "block";
-//   // } else {
-//   //   x.style.display = "none";
-//   // }
-// }
-function pop() {}
+function sendEmail() {
+  var name = $("#name");
+  var email = $("#email");
+  var message = $("#message");
+
+  if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(message)) {
+    $.ajax({
+      url: "mail.php",
+      method: "POST",
+      dataType: "json",
+      data: {
+        name: name.val(),
+        email: email.val(),
+        message: message.val(),
+      },
+      success: function (response) {
+        // $('#myForm')[0].reset();
+        // $('.sent-notification').text(response);
+        console.log(response);
+      },
+    });
+  }
+}
+
+function isNotEmpty(caller) {
+  if (caller.val() == "") {
+    caller.css("border", "1px solid red");
+    return false;
+  } else caller.css("border", "");
+  return true;
+}
